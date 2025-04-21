b184db3c5e5e3a12565be053627c3c5ccc734dbb
Apoorva Singh <apoorvasingh547@gmail.com>
2025-04-22 00:01:58 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-dotenv.config()
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
-  base:import.meta.env.VITE_BASE_PATH||"/Food_protoype",
-  css:{
+  base: import.meta.env.VITE_BASE_PATH || "/Food_protoype",
+  css: {
     modules: {
       localsConvention: 'camelCase',
     },
-  }
+  },
+  resolve: {
+    alias: {
+      '@fortawesome/react-fontawesome': resolve(__dirname, 'node_modules/@fortawesome/react-fontawesome'),
+    },
+  },
 })

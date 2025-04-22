d1c5c651675b679d7db0e4556cacc994d9dae74d
Apoorva Singh <apoorvasingh547@gmail.com>
2025-04-22 22:28:31 +0530

    Fix

--- src/components/Promobanner.jsx
+++ src/components/Promobanner.jsx
@@ -1,8 +1,8 @@
 // src/components/PromoBanner.jsx
 import React from 'react';
 import './PromoBanner.css';
-import L1 from '../images/L1.jpeg';
-import burgers from '../images/burgers.jpg';
+import L1 from '../images/L1.png';
+import R1 from '../images/R1.png';
 
 const PromoBanner = () => {
   return (
@@ -19,7 +19,7 @@ const PromoBanner = () => {
       </div>
 
       <div className="promo right">
-        <img src={burgers} alt="Promo Right" />
+        <img src={R1} alt="Promo Right" />
         <div className="promo-text">
           <h3>GET 40% OFF</h3>
           <p>
